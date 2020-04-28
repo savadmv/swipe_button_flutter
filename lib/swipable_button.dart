78cb860392e6025e9826c4e027c6b0a419ce8859
savad <Savadmv333@gmail.com>
2020-04-28 11:35:22 +0530

    Initial commit

---
+++ lib/swipable_button.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+/// A Widget that have the ability to detect swipe on itself
+/// with customizable variables.
+class SwipeableWidget extends StatefulWidget {
+
+  /// The `Widget` on which we want to detect the swipe movement.
+  final Widget child;
+  /// The Height of the widget that will be drawn, required.
+  final double height;
+  /// The `VoidCallback` that will be called once a swipe with certain percentage is detected.
+  final VoidCallback onSwipeCallback;
+  /// The decimal percentage of swiping in order for the callbacks to get called, defaults to 0.75 (75%) of the total width of the children.
+  final double swipePercentageNeeded;
+
+  final double screenSize;
+
+  final Function(bool,double) onSwipeStartcallback;
+
+  SwipeableWidget({
+    Key key,
+    @required this.child,
+    @required this.height,
+    @required this.onSwipeCallback,
+    @required this.onSwipeStartcallback,
+    @required this.screenSize,
+    this.swipePercentageNeeded = 0.75
+  }): assert(
+  child != null &&
+      onSwipeCallback != null &&
+      swipePercentageNeeded <= 1.0
+  ), super(key: key);
+
+  @override
+  _SwipeableWidgetState createState() => _SwipeableWidgetState();
+}
+
+class _SwipeableWidgetState extends State<SwipeableWidget> with SingleTickerProviderStateMixin {
+
+  AnimationController _controller;
+
+  var _dxStartPosition = 0.0;
+  var _dxEndsPosition = 0.0;
+  var _initControllerVal;
+
+  @override
+  void initState() {
+    super.initState();
+    _initControllerVal=widget.height/widget.screenSize;
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 300))
+      ..addListener(() {
+        setState(() {});
+        widget.onSwipeStartcallback(_controller.value>_initControllerVal+0.1,_controller.value);
+      });
+//    _controller.addStatusListener(());
+
+    _controller.value = _initControllerVal;
+
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onPanStart: (details) {
+          setState(() {
+            _dxStartPosition = details.localPosition.dx;
+          });
+        },
+        onPanUpdate: (details) {
+          final widgetSize = context.size.width;
+
+          // will only animate the swipe if user start the swipe in the quarter half start page of the widget
+          final minimumXToStartSwiping = widgetSize * 0.25;
+          if (_dxStartPosition <= minimumXToStartSwiping) {
+            setState(() {
+              _dxEndsPosition = details.localPosition.dx;
+            });
+
+            // update the animation value according to user's pan update
+            final widgetSize = context.size.width;
+            _controller.value = ((details.localPosition.dx) / widgetSize);
+          }
+//          widget.onSwipeStartcallback(_controller.value);
+        },
+        onPanEnd: (details) async {
+          // checks if the right swipe that user has done is enough or not
+          final delta = _dxEndsPosition - _dxStartPosition;
+          final widgetSize = context.size.width;
+          final deltaNeededToBeSwiped = widgetSize * widget.swipePercentageNeeded;
+          if (delta > deltaNeededToBeSwiped) {
+            // if it's enough, then animate to hide them
+            _controller.animateTo(1.0,
+                duration: Duration(milliseconds: 300), curve: Curves.fastOutSlowIn);
+            widget.onSwipeCallback();
+          } else {
+            // if it's not enough, then animate it back to its full width
+            _controller.animateTo(_initControllerVal,
+                duration: Duration(milliseconds: 300), curve: Curves.fastOutSlowIn);
+          }
+//          widget.onSwipeStartcallback(_controller.value);
+        },
+        child: Container(
+          height: widget.height,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: FractionallySizedBox(
+              widthFactor: _controller.value,
+              heightFactor: 1.0,
+              child: widget.child,
+            ),
+          ),
+        )
+    );
+  }
+}
---
+++ lib/swipebuttonflutter.dart
@@ -0,0 +1,180 @@
+library swipebuttonflutter;
+
+/// Swipe button
+import 'package:flutter/material.dart';
+import 'package:swipebuttonflutter/swipable_button.dart';
+
+
+/// A Button that can detect swiping movement with shimmering arrows on far end.
+class SwipingButton extends StatefulWidget {
+  /// The text that the button will display.
+  final String text;
+  final double width;
+
+  /// The callback invoked when the button is swiped.
+  final VoidCallback onSwipeCallback;
+
+  final Color swipeButtonColor;
+  final Color backgroundColor;
+  final Color iconColor;
+  TextStyle buttonTextStyle;
+
+  SwipingButton({
+    Key key,
+    @required this.text,
+    @required this.width,
+    @required this.onSwipeCallback,
+    this.swipeButtonColor = Colors.amber,
+    this.backgroundColor = Colors.black,
+    this.iconColor = Colors.white,
+    this.buttonTextStyle,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => StateSwipingButton(
+      text: text,
+      onSwipeCallback: onSwipeCallback,
+      width: width,
+      swipeButtonColor: this.swipeButtonColor,
+      backgroundColor: this.backgroundColor,
+      iconColor: this.iconColor,buttonTextStyle: this.buttonTextStyle);
+}
+
+class StateSwipingButton extends State<SwipingButton> {
+  /// The text that the button will display.
+  final String text;
+  final double width;
+
+  /// The callback invoked when the button is swiped.
+  final VoidCallback onSwipeCallback;
+  bool isSwiping = false;
+  double opacityVal = 1;
+  final Color swipeButtonColor;
+  final Color backgroundColor;
+  final Color iconColor;
+  TextStyle buttonTextStyle;
+
+  StateSwipingButton({
+    Key key,
+    @required this.text,
+    @required this.width,
+    @required this.onSwipeCallback,
+    this.swipeButtonColor = Colors.amber,
+    this.backgroundColor = Colors.black,
+    this.iconColor = Colors.white,
+    this.buttonTextStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (buttonTextStyle == null) {
+      buttonTextStyle = TextStyle(
+          fontSize: 16.0, fontWeight: FontWeight.w800, color: Colors.white);
+    }
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: width,
+            decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.circular(width / 2)),
+            child: new Center(
+              child: Text(
+                text.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          SwipeableWidget(
+            height: width,
+            screenSize: MediaQuery.of(context).size.width,
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: _buildContent(),
+              ),
+              height: width,
+              decoration: BoxDecoration(
+                  color: swipeButtonColor,
+                  borderRadius: BorderRadius.circular(width / 2)),
+            ),
+            onSwipeCallback: onSwipeCallback,
+            onSwipeStartcallback: (val, conVal) {
+              print("isGrate $conVal");
+              setState(() {
+                isSwiping = val;
+                opacityVal = 1 - conVal;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    return Flexible(
+      flex: 5,
+      child: Text(
+        text.toUpperCase(),
+        style: buttonTextStyle,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: AnimatedOpacity(
+                opacity:
+                (opacityVal - 0.2).isNegative ? 0.0 : (opacityVal - 0.2),
+                duration: Duration(milliseconds: 10),
+                child: Icon(
+                  Icons.chevron_right,
+                  color: iconColor,
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: AnimatedOpacity(
+                opacity:
+                (opacityVal - 0.2).isNegative ? 0.0 : (opacityVal - 0.2),
+                duration: Duration(milliseconds: 10),
+                child: Icon(
+                  Icons.chevron_right,
+                  color: iconColor,
+                ),
+              ),
+            ),
+            isSwiping
+                ? _buildText()
+                : Container(
+              width: 0,
+              height: 0,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
